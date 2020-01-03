04ea0f1633b4ec8ce6c111beef585d22e4de2533
Fernando J. Spitz <fspitz@technisys.com>
2020-01-03 19:16:33 -0300

    Se suaviza el movimiento y se agrega la animación para el movimiento de caída

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,7 +4,8 @@ var motion = Vector2()
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -500
 
 func _ready():
@@ -17,24 +18,36 @@ func _physics_process(delta):
 	# Gravity
 	motion.y += GRAVITY
 	
+	var friccion = false
+	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x -= ACCELERATION
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
 		$Sprite.play("Idle")
-		motion.x = 0
+		friccion = true
+		
 		
 	#print("Is on floor: " + str(is_on_floor()))
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friccion == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 100:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		
+		if friccion == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 	
 	motion = move_and_slide(motion, UP)
