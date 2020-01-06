185425d95c59b86499809ebc7678c218c4b8ad2b
Fernando J. Spitz <fjspitz@gmail.com>
2020-01-06 02:06:04 -0300

    Se agrega autotiles y parallax background

---
+++ WorldComplete.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var world_scene
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	
+	for body in bodies:
+		if body.name == 'Player':
+			get_tree().change_scene(world_scene)
